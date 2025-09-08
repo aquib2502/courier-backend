25bbb4502dbfe1f6b29f68455dd1e3cf0df6ee76
Resource-04 <resource4.code4bharat@gmail.com>
2025-09-08 12:30:44 +0530

    Test CI/CD again

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ app.use('/api/roles',roleRoutes )
 app.use('/api/rates',rateRoutes )
 
 const PORT =process.env.PORT
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
